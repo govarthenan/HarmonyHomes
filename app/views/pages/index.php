4c91d72faf0d6ee6ac50d606768502d6b0dd06c3
Himasha-2001 <2021is085@stu.ucsc.cmb.ac.lk>
2024-02-27 21:19:29 +0530

    landing page.

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -1,3 +1,58 @@
-<?php
+<?php ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo URL_ROOT . 'css/style.css'; ?>">
 
-echo "THIS IS THE HOMEPAGE OF PAGES";
+</head>
+<body>
+    <div class="main-container-landing">
+        <div class="landing-description">
+            <div class="description-text">
+                <div class="company-logo-and-name-landing">
+                  <img src="<?php echo URL_ROOT . 'resources/company-logo.png'; ?>" class="company-img" />
+                  <span class="company-name">Harmony Homes</span> <br />
+                </div>
+                <div class="subtitle">
+                  Where Convenience Meets Community!
+                </div>
+                <div class="welcome-message">
+                    <p>
+                        Step into a world of convenience and community with Castle Care.<br />
+                        More than just a platform, it's your key to a harmonious and connected<br />
+                        living experience within your residence. Here, we simplify and elevate your<br />
+                        residential living, transforming your home into a vibrant community.<br />
+                        Get ready to embrace a new era of effortless living with<span class="company-name">
+                            Harmony Homes!</span></p>
+                </div>
+            </div>
+
+          <?php
+              if ($_SERVER["REQUEST_METHOD"] == "POST") {
+              if (isset($_POST["login_button"])) {
+        
+              header("Location: login.php");
+              exit(); 
+              } elseif (isset($_POST["signup_button"])) {
+        
+              header("Location: signup.php");
+              exit(); 
+              }
+             }
+            ?>
+                <form method="post" class="description-buttons">
+                    <button type="submit" name="login_button" class="login_button">Sign In&nbsp;<span>&rsaquo;</span></button>
+                    <button type="submit" name="signup_button" class="signup_button">Sign Up&nbsp;<span>&rsaquo;</span></button>
+                </form>
+            
+        </div>
+        <div class="landing-graphics">
+            <img src="<?php echo URL_ROOT . 'resources/apartment.svg'; ?>" class="landing-img" />
+        </div>
+    </div>
+</body>
+</html>
+
