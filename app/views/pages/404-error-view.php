2c5af29511f0339d89ba405f2ceec34769d33ab6
Himasha-2001 <2021is085@stu.ucsc.cmb.ac.lk>
2024-02-27 21:20:25 +0530

    Added 404 error view page.

---
+++ app/views/pages/404-error-view.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo URL_ROOT . 'css/style.css'; ?>" />
+</head>
+<body>
+    <div class="main-container-404">
+      <img src="<?php echo URL_ROOT . 'resources/404-image.svg'; ?>" class="img-404">
+      <div class="page-not-found-heading">
+        Page not found!
+      </div>
+      <div class="page-not-found-description">
+        We're sorry,the page you requested<br> could not be found.please go back to<br> the home page!
+      </div>
+      <div class="home-button">Home Page</div>
+   </div>
+</body>
+</html>
