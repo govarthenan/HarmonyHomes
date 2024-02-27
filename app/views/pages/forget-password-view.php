282133f59e8b13d4137b76d06d36a35a2a32f6f3
Himasha-2001 <2021is085@stu.ucsc.cmb.ac.lk>
2024-02-27 21:20:04 +0530

    Added Forget password page.

---
+++ app/views/pages/forget-password-view.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="<?php echo URL_ROOT . 'css/style.css'; ?>"/>
+</head>
+<body>
+    <div class="main-container-forget-password">
+        <div class="forget-password-container">
+            <div class="forget-password-container-a">
+                <div class="forget-password-image-container">
+                    <img src="<?php echo URL_ROOT . 'resources/Questions-pana.svg' ?>" class="forget-password-image">
+                </div>
+                <div class="forget-password-title-container">
+                    <div class="forget-password-title">Forget password?</div>
+                </div>
+            </div>
+            <div class="forget-password-container-b">
+                <form method="post">
+                    <div class="form-forget-password">
+                        <label for="newPassword">Enter a new password:</label>
+                        <input type="password" id="newPassword" name="newPassword" required>
+                    </div>
+                    <div class="form-forget-password">
+                        <label for="confirmPassword">Re-type new password:</label>
+                        <input type="password" id="confirmPassword" name="confirmPassword" required>
+                    </div>
+                    <button type="submit" class="btn-save-forget-password">Save</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
