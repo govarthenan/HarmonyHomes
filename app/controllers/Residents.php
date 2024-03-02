964612543e943772c7b82fc2d762e7ccfe18c5d3
Govarthenan Rajadurai <govarthenan@gmail.com>
2024-03-02 16:05:40 +0530

    Convert data and error arrays as properties

--- app/controllers/Residents.php
+++ app/controllers/Residents.php
@@ -8,6 +8,8 @@
 class Residents extends Controller
 {
     private $model;
+    public $data = [];  // to store data entered by user, as well as to be passed to view
+    public $errors = [];  // to store errors, as well as to be passed to view
     public function __construct()
     {
         // load DB model
@@ -23,7 +25,7 @@ class Residents extends Controller
             // sanitize POST data
             $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-            $data = [
+            $this->data = [
                 'name' => trim($_POST['name']),
                 'email' => trim($_POST['email']),
                 'phone' => trim($_POST['phone']),
@@ -32,98 +34,97 @@ class Residents extends Controller
                 'floor_number' => trim($_POST['floor_number']),
                 'door_number' => trim($_POST['door_number']),
                 'nic' => trim($_POST['nic']),
-                'password' => trim($_POST['password']),
+                                'password' => trim($_POST['password']),
                 'confirm_password' => trim($_POST['confirm_password']),
             ];
 
             // array to store errors
-            $errors = [];
+            $this->errors = [];
 
             // validate input data
             // ToDo: add validation for floor, flat number limits
 
             // validate email
-            if (empty($data['email'])) {
-                $errors['email_err'] = 'Please enter email';
+            if (empty($this->data['email'])) {
+                $this->errors['email_err'] = 'Please enter email';
             } else {
-                if ($this->model->isEmailTaken($data['email'])) {
-                    $errors['email_err'] = 'Entered email is already registered.';
-                    echo $errors['email_err'];
+                if ($this->model->isEmailTaken($this->data['email'])) {
+                    $this->errors['email_err'] = 'Entered email is already registered.';
                 }
             }
 
             // validate name
-            if (empty($data['name'])) {
-                $errors['name_err'] = 'Please enter name';
+            if (empty($this->data['name'])) {
+                $this->errors['name_err'] = 'Please enter name';
             }
 
             // validate phone
-            if (empty($data['phone'])) {
-                $errors['phone_err'] = 'Please enter phone number';
-            } elseif (strlen($data['phone']) != 10) {
-                $errors['phone_err'] = 'Phone number must be 10 digits';
-            } elseif (!is_numeric($data['phone'])) {
-                $errors['phone_err'] = 'Phone number must be numeric';
-            } elseif ($data['phone'][0] != '0') {
-                $errors['phone_err'] = 'Phone number must start with 0';
+            if (empty($this->data['phone'])) {
+                $this->errors['phone_err'] = 'Please enter phone number';
+            } elseif (strlen($this->data['phone']) != 10) {
+                $this->errors['phone_err'] = 'Phone number must be 10 digits';
+            } elseif (!is_numeric($this->data['phone'])) {
+                $this->errors['phone_err'] = 'Phone number must be numeric';
+            } elseif ($this->data['phone'][0] != '0') {
+                $this->errors['phone_err'] = 'Phone number must start with 0';
             }
 
             // validate birthday
-            if (empty($data['birthday'])) {
-                $errors['birthday_err'] = 'Please enter birthday';
-            } elseif (strtotime($data['birthday']) > strtotime('today')) {
-                $errors['birthday_err'] = 'Birthday cannot be in the future';
+            if (empty($this->data['birthday'])) {
+                $this->errors['birthday_err'] = 'Please enter birthday';
+            } elseif (strtotime($this->data['birthday']) > strtotime('today')) {
+                $this->errors['birthday_err'] = 'Birthday cannot be in the future';
             }
 
             // validate gender
-            if (empty($data['gender'])) {
-                $errors['gender_err'] = 'Please select gender';
+            if (empty($this->data['gender'])) {
+                $this->errors['gender_err'] = 'Please select gender';
             }
 
             // validate floor number
-            if (empty($data['floor_number'])) {
-                $errors['floor_number_err'] = 'Please enter floor number';
-            } elseif (!is_numeric($data['floor_number'])) {
-                $errors['floor_number_err'] = 'Floor number must be numeric';
+            if (empty($this->data['floor_number'])) {
+                $this->errors['floor_number_err'] = 'Please enter floor number';
+            } elseif (!is_numeric($this->data['floor_number'])) {
+                $this->errors['floor_number_err'] = 'Floor number must be numeric';
             }
 
             // validate door number
-            if (empty($data['door_number'])) {
-                $errors['door_number_err'] = 'Please enter door number';
-            } elseif (!is_numeric($data['door_number'])) {
-                $errors['door_number_err'] = 'Door number must be numeric';
+            if (empty($this->data['door_number'])) {
+                $this->errors['door_number_err'] = 'Please enter door number';
+            } elseif (!is_numeric($this->data['door_number'])) {
+                $this->errors['door_number_err'] = 'Door number must be numeric';
             }
 
             // validate NIC
-            if (empty($data['nic'])) {
-                $errors['nic_err'] = 'Please enter NIC';
+            if (empty($this->data['nic'])) {
+                $this->errors['nic_err'] = 'Please enter NIC';
             }
 
             // validate password
-            if (empty($data['password'])) {
-                $errors['password_err'] = 'Please enter password';
-            } elseif (strlen($data['password']) < 6) {
-                $errors['password_err'] = 'Password must be at least 6 characters';
+            if (empty($this->data['password'])) {
+                $this->errors['password_err'] = 'Please enter password';
+            } elseif (strlen($this->data['password']) < 6) {
+                $this->errors['password_err'] = 'Password must be at least 6 characters';
             }
 
             // validate confirm password
-            if (empty($data['confirm_password'])) {
-                $errors['confirm_password_err'] = 'Please confirm password';
-            } elseif ($data['password'] != $data['confirm_password']) {
-                $errors['confirm_password_err'] = 'Passwords do not match';
+            if (empty($this->data['confirm_password'])) {
+                $this->errors['confirm_password_err'] = 'Please confirm password';
+            } elseif ($this->data['password'] != $this->data['confirm_password']) {
+                $this->errors['confirm_password_err'] = 'Passwords do not match';
             }
 
             // check if any errors exist by checking $errors array
             if (empty($errors)) {
                 // register user
-                echo 'no errors. lets register user';
+echo 'no errors. lets register user';
             } else {
-                die(print_r($errors));  // ToDO: improve error handling
+                die(print_r($this->errors));  // ToDO: improve error handling
             }
         } else {
             // load form
 
-            $this->loadView('residents/sign_up');
+            $this->loadView('residents/sign_up', $this->data);
         }
     }
 }
