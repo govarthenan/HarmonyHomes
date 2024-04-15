a5f3b31488a98d9f3af5b536f8210ee312a7cef8
Himasha-2001 <2021is085@stu.ucsc.cmb.ac.lk>
2024-04-15 16:26:29 +0530

    resident support page.

---
+++ app/views/residents/resident_support.php
@@ -0,0 +1,71 @@
+<?php
+include('sidenav.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <div class="main-content">
+      <div class="support-container">
+        <div class="support-center-heading">
+          Support Center
+        </div>
+        <div class="support-intro">
+         Welcome to our Support Center for the Flat Management System. We are dedicated to providing you with the assistance you need to ensure a seamless and enjoyable experience with our platform. Whether you have questions, concerns, or are facing any issues, our team is here to help. Please take advantage of the resources provided below to find the assistance you require.</p>
+        </div>
+          <div class="support-center-content">
+           <div class="column-1-support">
+             <div class="support-sub-heading">Contact Information:-</div>
+             <ul>
+               <li ><b>Phone Number:</b><i> +1 (555) 123-4567</i></li>
+               <li ><b>Email:</b><i> support@harmony.com</i></li>
+               <li><b>Assistance:</b> 070-1275832</i>
+               <li><b>Emergency Hotline:</b><i>+1 (555) 999-9999</i></li>
+            </ul>
+            <div class="support-sub-heading">Registration:-</div>
+              <div class="supoort-details">Each person who buys an apartment will get a user account of type "resident".
+                                           After validationg the identity of apartment buyer, registration of resident accounts is done by the receptionist at the receptionist desk.
+                                           Once the registration process completed, apartment owner will recieve an email of confirmation with his username and password.
+              </div>
+            <div class="support-sub-heading"> Common area Reservation:-</div>
+              <div class="supoort-details">Once logged in, visit the 'Amenity' section to view all available common area.Choose your desired date and time. Please adhere to the maximum time limits to ensure fair use for all residents.
+                                           Review your reservation details and confirm your booking. You will receive a confirmation email with your reservation details.
+              </div>
+              <div class="support-sub-heading">Maintenane and Technical Issue:-</div>
+                <div class="supoort-details">After logging in, navigate to the 'Issues' section. Provide a detailed description of the issue, including the location and any other relevant details.
+                                             Once you've filled out all necessary information, submit your request. You will receive a confirmation and can track the status of your request online.
+                </div>
+           </div>
+           <div class="column-2-support">
+           <div class="support-sub-heading">Technical Support:-</div>
+             <ul>
+              <li><b>Phone Number:</b><i>  +1 (555) 123-4567</i></li>
+              <li><b>Email:</b><i> techsupport@harmony.com</i></li>
+            </ul>
+            <div class="support-sub-heading">Login:-</div>
+             <div class="supoort-details">Apartment owner can login in to the system using username and password recieved to his/her email.
+                                          After the login into the system for the first time, it is his/her responsibility to change the password interms of security concerns.
+             </div>
+            <div class="support-sub-heading">Fitness Centre Reservation:-</div>
+              <div class="supoort-details">Once logged in, go to the Fitness Centre section to view the availability of the gym and equipment.
+                                           Select the desired date and time for your workout. Ensure your booking does not overlap with existing reservations.
+              </div>
+              <div class="support-sub-heading">Payments:-</div>
+                <div class="supoort-details">Access the payment portal where you can view your dues and payment history.
+                                             Select the payments you wish to make, whether it’s maintenance dues, amenity fees, or other charges.Enter your payment details securely and confirm the payment. A receipt will be emailed to you upon successful transaction.
+                </div>
+                <div class="support-sub-heading">Complaints:-</div>
+                <div class="supoort-details">Find the ‘Complaints’ section in the system after logging in.Provide a clear description of your complaint, specifying the location and any other relevant details.
+                                             After filling out the necessary information, submit your complaint. You will receive a tracking number for your reference.
+                </div>
+           </div>
+         </div>
+      </div>
+    </div>
+  </body>
+</html>
