3eeb647a234a25f04fd39880dfe5f99c8e722e2e
Himasha-2001 <2021is085@stu.ucsc.cmb.ac.lk>
2024-04-22 12:44:14 +0530

    fixed the complaint log styles.

--- app/views/residents/complaints_log.php
+++ app/views/residents/complaints_log.php
@@ -41,31 +41,7 @@ include(APP_ROOT . '/views/inc/resident_side_nav.php');
                             <h4 class="complaint-date-heading">Complaint Date</h4>
                             <p class="complaint-date"></p><?php echo $complaint->created_date; ?></p>
                         </div>
-                        <div class="date-column">
-                            <h4 class="complaint-date-heading">Attachment</h4>
-                            <p class="complaint-date"></p>
-                            <?php
 
-                            $blob = $complaint->attachments;
-
-                            if (empty($blob)) {
-                                echo "No Attachment";
-                            } else {
-                                $base64 = base64_encode($blob);
-
-                                // Determine the content type
-                                $finfo = new finfo(FILEINFO_MIME_TYPE);
-                                $type = $finfo->buffer($blob); // Get the MIME type of the blob data
-
-                                // Generate the src attribute for the img tag
-                                $imgSrc = 'data:' . $type . ';base64,' . $base64;
-
-                                // Output the img tag
-                                echo '<img width="65rem" src="' . $imgSrc . '" alt="Complaint Attachment">';
-                            }
-                            ?>
-                            </p>
-                        </div>
                         <div class="description-column">
                             <h4 class="complaint-type-heading"><?php echo $complaint->topic; ?></h4>
                             <p class="complaint-description"></p><?php echo $complaint->subject; ?></p>
@@ -78,13 +54,16 @@ include(APP_ROOT . '/views/inc/resident_side_nav.php');
 
                         <div class="file-column">
                             <h4 class="complaint-action-heading">Action</h4>
-                            <a href="<?php echo URL_ROOT . '/residents/complaintDetail/' . $complaint->complaint_id; ?>">
-                                <button class="viewButton">View</button>
-                            </a>
-                            <a href="<?php echo URL_ROOT . '/residents/complaintEdit/' . $complaint->complaint_id; ?>">
-                                <button class="updateButton">Update</button>
-                            </a>
+                            <div class="button-action">
+                                <a href="<?php echo URL_ROOT . '/residents/complaintDetail/' . $complaint->complaint_id; ?>">
+                                    <button class="viewButton">View</button>
+                                </a>
+                                <a href="<?php echo URL_ROOT . '/residents/complaintEdit/' . $complaint->complaint_id; ?>">
+                                    <button class="updateButton">Update</button>
+                                </a>
+                            </div>
                         </div>
+
 
 
 
