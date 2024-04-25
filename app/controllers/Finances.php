951c1fdbbb9d60c03588743c9c67177b66763c24
Govarthenan Rajadurai <govarthenan@gmail.com>
2024-04-26 04:30:50 +0530

    Fix billing CSV upload forms and implement validation

--- app/controllers/Finances.php
+++ app/controllers/Finances.php
@@ -1,5 +1,12 @@
 <?php
 
+// league/csv library
+
+use League\Csv\Reader;
+use League\Csv\Statement;
+
+require_once '../vendor/autoload.php';
+
 /**
  * Controller class for posts.
  *
@@ -87,8 +94,118 @@ class Finances extends Controller
         echo explode(' ', $user_name)[0];
     }
 
-    public function csvBilling()
+    /**
+     * Handles the CSV upload functionality.
+     *
+     * This method is responsible for handling the CSV file upload. It checks if the request method is POST,
+     * validates the uploaded CSV file, and processes it if there are no errors. If there are errors, it flushes
+     * the $_POST and $_FILES data and reloads the upload page.
+     *
+     * @return void
+     */
+    public function csvUpload()
     {
-        $this->loadView('finances/billing_csv');
-}
+        // check for post
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            // call function to validate csv
+            $validated_data = $this->preprocessCsv();
+
+            // if no errors, proceed to process the csv file
+            if ($validated_data) {
+                // process the csv file
+                echo var_dump($validated_data);
+            } else {
+                // if errors, flush $_POST and $_FILES data, and reload upload page
+                $_POST = [];
+                $_FILES = [];
+                header('Location: ' . URL_ROOT . '/finances/csvUpload');  // redirect to the same page
+            }
+        } else {
+            $this->loadView('finances/upload_csv');
+        }
+    }
+
+    /**
+     * Preprocesses the CSV file for water and electricity data.
+     *
+     * This method performs validation and preprocessing on the submitted CSV file.
+     * It filters and validates the $_POST and $_FILES data, checks the file type,
+     * validates the month and year, checks the billing type, and parses the CSV file.
+     *
+     * @return array|bool Returns an array containing the processed data if successful,
+     *                    or false if there are any errors.
+     */
+    public function preprocessCsv()
+    {
+        // same validation and preprocessing for both water and electricity data
+
+        // filter and validate $_POST data
+        $_POST = filter_var_array($_POST, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        // filter and validate $_FILES data
+        $_FILES = filter_var_array($_FILES, FILTER_SANITIZE_SPECIAL_CHARS);
+
+        // check file type and ensure it's a CSV file
+        if ($_FILES['billing_data']['type'] != 'text/csv') {
+            flash('error_csv_only_accepted', 'Only CSV files are accepted!', 'alert alert-error');  // schedule flash message
+            return false;
+        }
+
+        // validate the data
+
+        // check if month is valid
+        if ($_POST['month'] < 1 || $_POST['month'] > 12) {
+            flash('error_invalid_month', 'Invalid month!', 'alert alert-error');  // schedule flash message
+            return false;
+        }
+
+        // check if year is valid
+        // check for four digits and if its greater than the current year
+        if (strlen($_POST['year']) != 4 || $_POST['year'] > date('Y')) {
+            flash('error_invalid_year', 'Invalid year!', 'alert alert-error');  // schedule flash message
+            header('Location: ' . URL_ROOT . '/financespphp for/csvUpload');  // redirect to the same page
+        }
+
+        // check if billing type is valid
+        if ($_POST['billing_type'] != 'water' && $_POST['billing_type'] != 'power') {
+            flash('error_invalid_billing_type', 'Invalid billing type!', 'alert alert-error');  // schedule flash message
+            return false;
+        }
+
+        // using league/csv package, parse csv file
+        // get header, see if certain columns are present
+        // if not, error out
+
+        try {
+            $csv = Reader::createFromPath($_FILES['billing_data']['tmp_name'], 'r');
+            $csv->setHeaderOffset(0); //set line 1 as header
+
+            // get header columns
+            $header = $csv->getHeader();
+
+            // ensure follwing columns are present
+            $required_columns = ['floor', 'door', 'amount'];
+
+            // check if required columns are present
+            foreach ($required_columns as $column) {
+                if (!in_array($column, $header)) {
+                    $this->errors['missing_column'] = 'Missing column: ' . $column;
+                    flash('error_missing_column', 'Missing column: ' . $column, 'alert alert-error');  // schedule flash message
+                    return false;
+                }
+            }
+
+            // if no errors, collect other data and proceed to process the csv file
+            $data['csv'] = $csv;  // store csv object
+            $data['month'] = $_POST['month'];
+            $data['year'] = $_POST['year'];
+            $data['billing_type'] = $_POST['billing_type'];
+
+            // if all is fine, return processed data
+            return $data;
+        } catch (Throwable $e) {
+            flash('error_csv_parsing', 'Unknown error in CSV, please check and reupload!', 'alert alert-error');  // schedule flash message
+            return false;
+        }
+    }
 }
--- app/views/finances/upload_csv.php
+++ app/views/finances/upload_csv.php
@@ -14,30 +14,98 @@ include(APP_ROOT . '/views/inc/finance_side_nav.php');
 <body onload="randerDate()">
     <div class="main-content">
         <div class="billing-container-finance">
+            <?php
+            // call all flash messages
+            flash('error_csv_only_accepted');
+            flash('error_invalid_month');
+            flash('error_invalid_year');
+            flash('error_invalid_billing_type');
+            flash('error_missing_column');
+            flash('error_csv_parsing')
+            ?>
+
             <div class="billing-heading">Issue monthly bills</div>
             <div class="billing-container">
-                <form action="upload_csv.php" method="POST">
-                    <div class="elec-container">
+
+                <!-- water form -->
+                <div class="elec-container">
+                    <form enctype="multipart/form-data" action="<?php echo URL_ROOT . '/finances/csvUpload' ?>" method="POST">
                         <label for="waterFile">Water Data (CSV):</label>
                         <div class="file-upload-wrapper">
-                            <input type="file" id="file-input" name="electricityFile" accept=".csv" required hidden />
+                            <input type="file" id="file-input" name="billing_data" accept=".csv" required hidden />
                             <label for="file-input" class="file-input-label">Choose File</label>
                             <span id="file-name">No file chosen...</span>
                         </div>
-                    </div>
-                    <div class="elec-container">
-                        <label for="waterFile">Water Data (CSV):</label>
+
+                        <p> select date for the data</p>
+
+                        <!-- month input -->
+                        <select name="month" required>
+                            <option value="1">January</option>
+                            <option value="2">February</option>
+                            <option value="3">March</option>
+                            <option value="4">April</option>
+                            <option value="5">May</option>
+                            <option value="6">June</option>
+                            <option value="7">July</option>
+                            <option value="8">August</option>
+                            <option value="9">September</option>
+                            <option value="10">October</option>
+                            <option value="11">November</option>
+                            <option value="12">December</option>
+                        </select>
+
+                        <!-- year input -->
+                        <input type="number" name="year" id="year" min="<?php echo date('Y') ?>" max="<?php echo date('Y') + 1 ?>" required placeholder="Year" />
+
+                        <!-- billing type indicator, hidden input -->
+                        <input type="hidden" name="billing_type" value="water" />
+
+                        <button type="submit" class="uploadButton">Upload Files</button>
+                    </form>
+                </div>
+
+                <!-- electricity form -->
+                <div class="elec-container">
+                    <form enctype="multipart/form-data" action="<?php echo URL_ROOT . '/finances/csvUpload' ?>" method="POST">
+                        <label for="waterFile">Electricity Data (CSV):</label>
                         <div class="file-upload-wrapper">
-                            <input type="file" id="file-input" name="electricityFile" accept=".csv" required hidden />
+                            <input type="file" id="file-input" name="billing_data" accept=".csv" required hidden />
                             <label for="file-input" class="file-input-label">Choose File</label>
                             <span id="file-name">No file chosen...</span>
                         </div>
-                    </div>
-                    <button type="submit" class="uploadButton">Upload Files</button>
+
+                        <p> select date for the data</p>
+
+                        <!-- month input -->
+                        <select name="month" required>
+                            <option value="1">January</option>
+                            <option value="2">February</option>
+                            <option value="3">March</option>
+                            <option value="4">April</option>
+                            <option value="5">May</option>
+                            <option value="6">June</option>
+                            <option value="7">July</option>
+                            <option value="8">August</option>
+                            <option value="9">September</option>
+                            <option value="10">October</option>
+                            <option value="11">November</option>
+                            <option value="12">December</option>
+                        </select>
+
+                        <!-- year input -->
+                        <input type="number" name="year" id="year" min="<?php echo date('Y') ?>" max="<?php echo date('Y') + 1 ?>" required placeholder="Year" />
+
+                        <!-- billing type indicator, hidden input -->
+                        <input type="hidden" name="billing_type" value="power" />
+
+                        <button type="submit" class="uploadButton">Upload Files</button>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
-    <script src="index.js"></script>
+    <script src="<?php echo URL_ROOT . '/js/index.js' ?>"></script>
 </body>
 
 </html>
