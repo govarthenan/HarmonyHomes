f046f8928ae0d6e755f68cbbdbed0a5d15e3274f
Shanthos <pshanthos12@gmail.com>
2024-04-26 07:34:11 +0530

    technician function

---
+++ app/models/Facility.php
@@ -0,0 +1,99 @@
+<?php
+
+/**
+ * Specialized version (ideally) of the Database class for the Post model.
+ *
+ * @property mixed $db An instance of the Database class.
+ */
+class Facility
+{
+    private $db;
+    public $primary_table = 'staff';
+
+    public function __construct()
+    {
+        $this->db = new Database;
+    }
+
+    /**
+     * Retrieves the display name of a user based on their ID.
+     *
+     * @param int $user_id The ID of the user.
+     * @return string The full name of the user.
+     */
+    public function getUserDisplayName(int $user_id)
+    {
+        $this->db->prepareQuery("SELECT name FROM {$this->primary_table} WHERE user_id = :id");
+        $this->db->bind('id', $user_id);
+
+        $row = $this->db->singleResult();
+
+        return $row->name;  // return string full name
+    }
+
+    public function getIssues()
+    {
+        $this->db->prepareQuery('SELECT i.*, r.floor_number, r.door_number
+        FROM issue i
+        JOIN resident r ON r.user_id = i.user_id
+        ');
+        // $this->db->bind('user_id', $_SESSION['user_id']);
+        $this->db->resultSet();
+        return $this->db->resultSet();
+        
+    
+    }
+    public function addTechnician($data){
+        $this->db->prepareQuery('INSERT INTO `technician_overview`( first_name, last_name, specialization, experience, email, phone_number) VALUES (:first_name, :last_name, :specialization, :experience, :email, :phone_number)');
+        // Bind values
+        
+        $this->db->bind('first_name', $data['first_name']);
+        $this->db->bind('last_name', $data['last_name']);
+        $this->db->bind('specialization', $data['specialization']);
+        $this->db->bind('experience', $data['experience']);
+        $this->db->bind('email', $data['email']);
+        $this->db->bind('phone_number', $data['phone_number']);
+
+       
+
+        // Execute the query and return bool
+        return $this->db->execute();
+        
+
+    }
+
+    public function viewTechnician(){
+        $this->db->prepareQuery('SELECT * FROM `technician_overview` ');
+        // Bind values
+   
+        // Execute the query and return bool
+        return $this->db->resultSet();
+    }
+
+    public function fetchTechnicianDetails(int $technician_id)
+    {
+        $this->db->prepareQuery('SELECT * FROM technician_overview  WHERE technician_id = :technician_id');
+        $this->db->bind('technician_id', $technician_id);
+
+        $row = $this->db->singleResult();
+
+        if ($this->db->rowCount() > 0) {
+            return $row;
+        } else {
+            return false;
+        }
+    }
+    public function deleteTechnician($technician_id){
+        $this->db->prepareQuery('DELETE FROM technician_overview
+        WHERE technician_id = :technician_id;');
+        // Bind values
+        $this->db->bind('technician_id',$technician_id);
+        // Execute the query and return bool
+        return $this->db->execute();
+    }
+    // fetch technician detail
+   
+   
+
+
+}
