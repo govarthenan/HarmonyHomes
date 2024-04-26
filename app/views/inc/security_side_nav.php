f4e1dcf40689005fe43b33a471ad3a4c2bea8a80
Himasha-2001 <2021is085@stu.ucsc.cmb.ac.lk>
2024-04-26 13:34:04 +0530

    Add resources for the side nav  path.

--- app/views/inc/security_side_nav.php
+++ app/views/inc/security_side_nav.php
@@ -24,44 +24,44 @@
 
             <div class="dropdown-content">
                 <div class="dropdown-title">
-                    <img src="<?php echo URL_ROOT . '/resources/security/sidebar_user.svg' ?>" class="sidebar-user">
+                    <img src="<?php echo URL_ROOT . '/resources/securities/sidebar_user.svg' ?>" class="sidebar-user">
                     <span class="dropdown-item">Profile</span>
                 </div>
 
                 <div class="dropdown-title">
-                    <img src="<?php echo URL_ROOT . '/resources/security/sidebar_options.svg' ?>" class="sidebar-options">
+                    <img src="<?php echo URL_ROOT . '/resources/securities/sidebar_options.svg' ?>" class="sidebar-options">
                     <span class="dropdown-item">Settings</span>
                 </div>
             </div>
         </div>
         <nav class="navigation">
             <div class="navi-item">
-                <img src="<?php echo URL_ROOT . '/resources/security/sidebar_amenities.svg' ?>">
+                <img src="<?php echo URL_ROOT . '/resources/securities/sidebar_amenities.svg' ?>">
                 <span class="sidebar-title">Home</span>
             </div>
             <div class="navi-item">
-                <img src="<?php echo URL_ROOT . '/resources/security/visitor.svg' ?>" class="sidebar_icon">
+                <img src="<?php echo URL_ROOT . '/resources/securities/visitor.svg' ?>" class="sidebar_icon">
                 <span class="sidebar-title">Visitor Tracking</span>
             </div>
 
             <div class="navi-item">
-                <img src="<?php echo URL_ROOT . '/resources/security/external.svg' ?>" class="sidebar_icon">
+                <img src="<?php echo URL_ROOT . '/resources/securities/external.svg' ?>" class="sidebar_icon">
                 <span class="sidebar-title">External Personal</span>
             </div>
 
             <div class="navi-item">
-                <img src="<?php echo URL_ROOT . '/resources/security/delivery.svg' ?>" class="sidebar_icon">
+                <img src="<?php echo URL_ROOT . '/resources/securities/delivery.svg' ?>" class="sidebar_icon">
                 <span class="sidebar-title">Apartment Deliveries</span>
             </div>
 
             <div class="navi-item">
-                <img src="<?php echo URL_ROOT . '/resources/security/sidebar_help.svg' ?>">
+                <img src="<?php echo URL_ROOT . '/resources/securities/sidebar_help.svg' ?>">
                 <span class="sidebar-title">Support</span>
             </div>
 
             <hr />
             <div class="navi-item">
-                <img src="<?php echo URL_ROOT . '/resources/security/sidebar_log-out.svg' ?>">
+                <img src="<?php echo URL_ROOT . '/resources/securities/sidebar_log-out.svg' ?>">
                 <span class="sidebar-title">Logout</span>
             </div>
         </nav>
