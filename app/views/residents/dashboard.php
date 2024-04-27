12c0160b101b5e98ab0a6714f3ac62a24a8ce533
Govarthenan Rajadurai <govarthenan@gmail.com>
2024-04-27 15:07:35 +0530

    Fix formatting

--- app/views/residents/dashboard.php
+++ app/views/residents/dashboard.php
@@ -1,38 +1,40 @@
 <?php
-include(APP_ROOT.'/views/inc/resident_side_nav.php');
+include(APP_ROOT . '/views/inc/resident_side_nav.php');
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
     <link rel="stylesheet" href="<?php echo URL_ROOT . '/css/style.css'; ?>" />
-  </head>
-  <body onload="randerDate()">
+</head>
+
+<body onload="randerDate()">
     <div class="main-content">
-      <div class="dashboard-container">
-        <div class="first-row-resident-dashboard">
-          <div class="announcment-container">
-            <div class="announcment-heading">
-              Announcment
-            </div>
-            
-            <div class="announcment-tile-container">
-               <div class="announcment-1">
-                 <div class="announcment-tile-heading">Monthly General meeting</div>
-                      <!--<div class="announcment-details">Dear residents,<br>
-                         We are excited to announce our general meeting for the month of February.<br>
-                        <b>Date:2024.02.27</b><br>
-                        <b>Time:8.00 a.m</b><br>
-                        <b>Location:</b><br>Please make sure to attend and participate. Your presence is highly valued!<br>
-                         Thank you,<br>
-                         General manager.
-                      </div>-->
-                   </div>
-               <div class="announcment-2">
-                 <div class="announcment-tile-heading">Power Cut Announcement</div>
-                      <!--<div class="announcment-details">Dear residents,<br>
+        <div class="dashboard-container">
+            <div class="first-row-resident-dashboard">
+                <div class="announcment-container">
+                    <div class="announcment-heading">
+                        Announcment
+                    </div>
+
+                    <div class="announcment-tile-container">
+                        <div class="announcment-1">
+                            <div class="announcment-tile-heading">Monthly General meeting</div>
+                            <div class="announcment-details">Dear residents,<br>
+                                We are excited to announce our general meeting for the month of February.<br>
+                                <b>Date:2024.02.27</b><br>
+                                <b>Time:8.00 a.m</b><br>
+                                <b>Location:</b><br>Please make sure to attend and participate. Your presence is highly valued!<br>
+                                Thank you,<br>
+                                General manager.
+                            </div>
+                        </div>
+                        <div class="announcment-2">
+                            <div class="announcment-tile-heading">Power Cut Announcement</div>
+                            <!--<div class="announcment-details">Dear residents,<br>
                       We would like to inform you about a scheduled power cut on the 4th of February.<br>
                         <b>Date:2024.02.27</b><br>
                         <b>Time:8.00 a.m</b><br>
@@ -41,106 +43,107 @@ include(APP_ROOT.'/views/inc/resident_side_nav.php');
                          Thank you,<br>
                          Facility manager.
                       </div>-->
-                   </div>
+                        </div>
+                    </div>
                 </div>
-          </div>
-          <div class="calendar">
-            <div class="calendar-container">
-              <div class="month">
-               <div class="prev" onclick="moveDate('prev')">
-                <span class="arrow">&#10094</span>
-              </div>
+                <div class="calendar">
+                    <div class="calendar-container">
+                        <div class="month">
+                            <div class="prev" onclick="moveDate('prev')">
+                                <span class="arrow">&#10094</span>
+                            </div>
 
-              <div>
-                <h2 id="month">April-2023</h2>
-                <p id="date">Tue April 20 2023</p>
-              </div>
+                            <div>
+                                <h2 id="month">April-2023</h2>
+                                <p id="date">Tue April 20 2023</p>
+                            </div>
 
-              <div class="next" onclick="moveDate('next')">
-               <span class="arrow">&#10095</span>
-              </div>
+                            <div class="next" onclick="moveDate('next')">
+                                <span class="arrow">&#10095</span>
+                            </div>
+                        </div>
+
+                        <div class="week">
+                            <div>Sun</div>
+                            <div>Mon</div>
+                            <div>Tue</div>
+                            <div>Wed</div>
+                            <div>Thu</div>
+                            <div>Fri</div>
+                            <div>Sat</div>
+                        </div>
+
+                        <div class="dates">
+                        </div>
+                    </div>
+                </div>
             </div>
 
-            <div class="week">
-              <div>Sun</div>
-              <div>Mon</div>
-              <div>Tue</div>
-              <div>Wed</div>
-              <div>Thu</div>
-              <div>Fri</div>
-              <div>Sat</div>
+            <div class="second-row-resident-dashboard">
+                <div class="booking-reminder">
+                    <img src="<?php echo URL_ROOT . '/resources/resident_dashboard/waiting-appointments-calendar.svg' ?>" class="booking-reminder-img">
+                    <div class="reminder-heading">Booking</div>
+                </div>
+                <div class="event-reminder">
+                    <img src="<?php echo URL_ROOT . '/resources/resident_dashboard/calendar-star.svg' ?>" class="event-reminder-img">
+                    <div class="reminder-heading">Events</div>
+                </div>
+                <div class="payment-reminder">
+                    <img src="<?php echo URL_ROOT . '/resources/resident_dashboard/subscription-cashflow.svg' ?>" class="payment-reminder-img">
+                    <div class="reminder-heading">Payments</div>
+                </div>
+                <div class="private-files">
+                    <img src="<?php echo URL_ROOT . '/resources/resident_dashboard/business-user-curriculum.svg' ?>" class="business-user-curriculum-img">
+                    <div class="private-files-heading">Private files</div>
+                </div>
             </div>
-            
-            <div class="dates">
+            <div class="third-row-resident-dashboard">
+                <div class="maitenance-payment-overview">
+                    <canvas id="maintenancePaymentChart" width="1500%" height="200%"></canvas>
+                </div>
+                <div class="utility-useage-overview">
+                    <canvas id="utilityUsageChart" width="100%" height="90%"></canvas>
+                </div>
             </div>
-           </div>
-          </div>
         </div>
-  
-        <div class="second-row-resident-dashboard">
-          <div class="booking-reminder">
-            <img src="<?php echo URL_ROOT . '/resources/resident_dashboard/waiting-appointments-calendar.svg'?>" class="booking-reminder-img">
-            <div class="reminder-heading">Booking</div>
-          </div>
-          <div class="event-reminder">
-             <img src="<?php echo URL_ROOT . '/resources/resident_dashboard/calendar-star.svg'?>" class="event-reminder-img">
-             <div class="reminder-heading">Events</div>
-          </div>
-          <div class="payment-reminder">
-            <img src="<?php echo URL_ROOT . '/resources/resident_dashboard/subscription-cashflow.svg'?>" class="payment-reminder-img">
-            <div class="reminder-heading">Payments</div>
-          </div>
-          <div class="private-files">
-           <img src="<?php echo URL_ROOT . '/resources/resident_dashboard/business-user-curriculum.svg'?>" class="business-user-curriculum-img">
-           <div class="private-files-heading">Private files</div>
-          </div>
-        </div>
-        <div class="third-row-resident-dashboard">
-          <div class="maitenance-payment-overview">
-             <canvas id="maintenancePaymentChart" width="1500%" height="200%"></canvas>
-          </div>
-          <div class="utility-useage-overview">
-             <canvas id="utilityUsageChart" width="100%" height="90%"></canvas>
-          </div>
-        </div>
-      </div>
     </div>
 
     <!-- modal part -->
-        
+
     <div id="popupModal-booking" class="popup-booking" style="display:none;">
-       <div class="modal-content-booking">
-         <span class="close">&times;</span>
-         <div class="bookin-msg">Fitness center<br>12/02/2024<br>10.30 a.m</div>
-       </div>
+        <div class="modal-content-booking">
+            <span class="close">&times;</span>
+            <div class="bookin-msg">Fitness center<br>12/02/2024<br>10.30 a.m</div>
+        </div>
     </div>
 
     <div id="popupModal-events" class="popup-events" style="display:none;">
-       <div class="modal-content-events">
-         <span class="close">&times;</span>
-         <div class="event-msg">Sinhala and tamil new year celebration<br>11/04/2024<br>10.30 a.m</div>
-       </div>
+        <div class="modal-content-events">
+            <span class="close">&times;</span>
+            <div class="event-msg">Sinhala and tamil new year celebration<br>11/04/2024<br>10.30 a.m</div>
+        </div>
     </div>
 
     <div id="popupModal-payment" class="popup-payment" style="display:none;">
-       <div class="modal-content-payment">
-         <span class="close">&times;</span>
-         <div class="event-msg">Maintenace fees<br>11/04/2024<br>11.55 p.m</div>
-       </div>
+        <div class="modal-content-payment">
+            <span class="close">&times;</span>
+            <div class="event-msg">Maintenace fees<br>11/04/2024<br>11.55 p.m</div>
+        </div>
     </div>
 
     <div id="popupModal-announcement" class="popup-announcement" style="display:none;">
-       <div class="modal-content-announcement">
-         <span class="close">&times;</span>
-         <div class="announcement-msg">
-          <div class="title">Power Cut</div>
-          <div class="sender">Facility manager</div>
-          <div class="date">sent date</div>
-          <div class="message">fhrhgruhgiergr</div>
-       </div>
-    </div>
+        <div class="modal-content-announcement">
+            <span class="close">&times;</span>
+            <div class="announcement-msg">
+                <div class="title">Power Cut</div>
+                <div class="sender">Facility manager</div>
+                <div class="date">sent date</div>
+                <div class="message">fhrhgruhgiergr</div>
+            </div>
+        </div>
 
-    <script src="<?php echo URL_ROOT . '/js/index.js'; ?>" ></script>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-  </body>
+        <script src="<?php echo URL_ROOT . '/js/index.js'; ?>"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+</body>
+
 </html>
